c1a3d7255e178f42f6df08d5125ea07f1d8a3d3c
Raspberry Tech <noobie.mine@gmail.com>
2021-03-03 18:30:02 +0000

    added comments

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const express = require('express');
-const https = require('https');
-const fs = require('fs');
-const rateLimit = require('express-rate-limit');
-const helmet = require('helmet'); 
-const str = require('@supercharge/strings')
-const { default: contentSecurityPolicy } = require('helmet/dist/middlewares/content-security-policy');
-const { response } = require('express');
-var mysql = require('mysql');  
-var con = mysql.createConnection({  
-    host:'localhost',  
-    user:'xx',  
-    Password:'xx',  
-    database:'congkak'  
+const express = require('express'); //used in running the express API server
+const https = require('https'); //used for securing the API route
+const fs = require('fs'); //filesystem module
+const rateLimit = require('express-rate-limit'); //library to set a rate limit on routes
+const helmet = require('helmet');  //used to help prevent against API attacks
+const str = require('@supercharge/strings') //used for generating random strings
+const { default: contentSecurityPolicy } = require('helmet/dist/middlewares/content-security-policy');//helmet middleware
+var mysql = require('mysql');  //mysql database library
+var con = mysql.createConnection({  //creates the mysql connection
+    host:'localhost',  //host
+    user:'xx',   //username
+    Password:'xx',   //password
+    database:'congkak'  //database
 }); 
-con.connect(function(err) {
+con.connect(function(err) { //try and connect
     if (err) throw err;
     console.log("Connected!");
 });
@@ -22,7 +21,7 @@ const serverName = "xx"; //server name - the domain name, xx.domainname.com
 var privateKey  = fs.readFileSync('/etc/letsencrypt/live/' + serverName + '.sunnahvpn.com/privkey.pem', 'utf8'); //location to https private key
 var certificate = fs.readFileSync('/etc/letsencrypt/live/' + serverName + '.sunnahvpn.com/fullchain.pem', 'utf8'); //location to fullchain key
 var credentials = {key: privateKey, cert: certificate};
-const app = express();
+const app = express(); //instantiate express
 
 app.use(express.json({ limit: '10kb' })); // Body limit is 10
 //SECURITY START 
@@ -38,16 +37,16 @@ app.use(helmet());
 //SECURITY END
 //API START
 
-app.post('/api/login', async function (req, res) {
-    let Username = req.body.Username; //TEST START
+app.post('/api/login', async function (req, res) { //API route
+    let Username = req.body.Username; 
     let Password = req.body.Password;
-    console.log(req.body);
+    console.log(req.body); //TEST START
     console.log("Username:" + Username);
     console.log("Password:" + Password); //TEST END
 
-    let func = await login(Username, Password);
+    let func = await login(Username, Password);//login user
 
-    let response = { //test for JSON sending
+    let response = { 
         ApiResponse: func[0],
         UserID: func[1],
         AuthKey: func[2],
@@ -58,13 +57,13 @@ app.post('/api/login', async function (req, res) {
     res.send(JSON.stringify(response)); 
 });
 app.post('/api/register', async function (req, res) {
-    let Username = req.body.Username; //TEST START
+    let Username = req.body.Username; 
     let Password = req.body.Password;
-    console.log(req.body);
+    console.log(req.body); //TEST START
     console.log("Username:" + Username);
-    console.log("Password:" + Password);//TEST  END
+    console.log("Password:" + Password); //TEST END
     
-    let func = await register(Username, Password);
+    let func = await register(Username, Password); //register user
     let response = { //test for JSON sending
         ApiResponse: func[0],
         UserID: func[1]
@@ -72,16 +71,16 @@ app.post('/api/register', async function (req, res) {
     res.send(JSON.stringify(response)); 
 });
 
-app.post('/api/getinfo', async function (req, res) {
-    let Username = req.body.Username; //TEST START
+app.post('/api/getinfo', async function (req, res) { 
+    let Username = req.body.Username; 
     let UserID = req.body.UserID; 
     let AuthKey = req.body.AuthKey;
-    console.log(req.body);
+    console.log(req.body);//TEST START
     console.log("UserID:" + UserID);
     console.log("AuthKey:" + AuthKey);//TEST  END
     
-    let func = await getInfo(Username, UserID, AuthKey);
-    let response = { //test for JSON sending
+    let func = await getInfo(Username, UserID, AuthKey); //get user info
+    let response = { 
         ApiResponse: func[0],
         UserGameID: func[1],
         UserSave: func[2],
@@ -91,7 +90,7 @@ app.post('/api/getinfo', async function (req, res) {
     res.send(JSON.stringify(response)); 
 });
 app.post('/api/savegame', async function (req, res) {
-    let Username = req.body.Username; //TEST START
+    let Username = req.body.Username; 
     let UserID = req.body.UserID; 
     let AuthKey = req.body.AuthKey;
 
@@ -99,30 +98,30 @@ app.post('/api/savegame', async function (req, res) {
     let AISave = req.body.AISave;
     let WhichTurn = req.body.WhichTurn;
 
-    let func = await saveGame(Username, UserID, AuthKey, UserSave, AISave, WhichTurn);
-    let response = { //test for JSON sending
+    let func = await saveGame(Username, UserID, AuthKey, UserSave, AISave, WhichTurn); //save user's game
+    let response = { 
         ApiResponse: func[0]
     };
     res.send(JSON.stringify(response)); 
 });
 
-app.post('/api/savedata', async function (req, res) {
-    let Username = req.body.Username; //TEST START
+app.post('/api/savedata', async function (req, res) { 
+    let Username = req.body.Username; 
     let UserID = req.body.UserID; 
     let AuthKey = req.body.AuthKey;
     let Shells = req.body.Shells;
     let Win = req.body.Win;
 
-    let func = await updateData(Username, UserID, AuthKey, Shells, Win);
-    let response = { //test for JSON sending
+    let func = await updateData(Username, UserID, AuthKey, Shells, Win); //update data after game
+    let response = { 
         ApiResponse: func[0]
     };
     res.send(JSON.stringify(response)); 
 });
 
 app.post('/api/leaderboard', async function (req, res) {
-    let func = await getLeaderboard();
-    let response = { //test for JSON sending
+    let func = await getLeaderboard(); //get leaderboard data
+    let response = { 
         UserOneName: func[0],
         UserOnePoints: func[1],
         UserTwoName: func[2],
@@ -134,7 +133,7 @@ app.post('/api/leaderboard', async function (req, res) {
 });
 
 
-const query = (q) => new Promise((resolve, reject) => {
+const query = (q) => new Promise((resolve, reject) => { //function for running queries on the database
     con.query(q, function (err, result, fields) {
       if (err) {
         reject(err);
@@ -145,21 +144,21 @@ const query = (q) => new Promise((resolve, reject) => {
     });
   }); 
   
-async function login(Username, Password) {  
+async function login(Username, Password) {  //function for logging in user
     //DATE
     try{
-        let date_ob = new Date();
+        let date_ob = new Date(); //split date into parts
         let year = date_ob.getFullYear();
         let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-        let date = ("0" + date_ob.getDate()).slice(-2);
+        let date = ("0" + date_ob.getDate()).slice(-2); 
         //QUERY
-        let queryOne = `SELECT * FROM authentication WHERE Username = "${Username}";`;
+        let queryOne = `SELECT * FROM authentication WHERE Username = "${Username}";`; //get all data about user
         let selectResultOne = await query(queryOne);
-        if(selectResultOne.length > 0){
+        if(selectResultOne.length > 0){ //if user is found
             try{
                 let PasswordResult = selectResultOne[0].Password;
-                let authKey = str.random(32); //random 32 bit value
-                if (PasswordResult == Password) {
+                let authKey = str.random(32); //random 32 bit value for authorization key
+                if (PasswordResult == Password) { //if passwords match
                     
                     var updateOne = `UPDATE authentication SET LastLogin = "${year}-${month}-${date}", AuthKey = "${authKey}" WHERE Username = "${Username}";`;
                     await query(updateOne);
@@ -170,7 +169,6 @@ async function login(Username, Password) {
                     let wins = selectResultTwo[0].Wins;
                     let losses = selectResultTwo[0].Losses;
                     let totalScore = selectResultTwo[0].TotalScore;
-                    //add user game ID
                     return([true, userIDResult, authKey, wins, losses, totalScore]);
                 }
                 else{
@@ -188,7 +186,7 @@ async function login(Username, Password) {
     }
 }
 
-async function register(Username, Password){
+async function register(Username, Password){ //function for registering
     let date_ob = new Date();
     let year = date_ob.getFullYear();
     let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
@@ -196,7 +194,7 @@ async function register(Username, Password){
     let authKey = str.random(32); //random 32 bit value
     try{
         var insertOne = `INSERT INTO authentication (Username, Password, LastLogin, AuthKey) VALUES ("${Username}", "${Password}", "${year}-${month}-${date}", "${authKey}");`; 
-        await query(insertOne);
+        await query(insertOne); //tries and insert new user
 
         var insertTwo = `INSERT INTO player(Username, Wins, Losses, TotalScore) VALUES ("${Username}", 0, 0, 0);`;
         await query(insertTwo);
@@ -205,12 +203,12 @@ async function register(Username, Password){
         selectResultOne = await query(selectOne);
         return([true, selectResultOne[0].UserID]);
     }
-    catch(err){
+    catch(err){ //if an error occurs
         console.log(err);
         return([false, false]);
     }
 }
-async function getInfo(Username, UserID, AuthKey){
+async function getInfo(Username, UserID, AuthKey){ //get user saved game from DB
     try{
         var queryOne = `SELECT AuthKey FROM authentication WHERE Username = "${Username}";`;
         let selectResultOne = await query(queryOne);
@@ -241,14 +239,14 @@ async function getInfo(Username, UserID, AuthKey){
     }
 }
 
-async function saveGame(Username, UserID, AuthKey, UserOneShells, UserTwoShells, WhichTurn){
+async function saveGame(Username, UserID, AuthKey, UserOneShells, UserTwoShells, WhichTurn){ //save a user's game
     try{
         var queryOne = `SELECT AuthKey FROM authentication WHERE Username = "${Username}";`;
         let selectResultOne = await query(queryOne);
         if (selectResultOne.length > 0){
             let authKeyResult = selectResultOne[0].AuthKey;
             if(authKeyResult == AuthKey){
-                date = new Date();
+                date = new Date(); //had to use UTC as had some issues with saving in database
                 date = date.getUTCFullYear() + '-' +
                     ('00' + (date.getUTCMonth()+1)).slice(-2) + '-' +
                     ('00' + date.getUTCDate()).slice(-2) + ' ' + 
@@ -270,7 +268,7 @@ async function saveGame(Username, UserID, AuthKey, UserOneShells, UserTwoShells,
         return([false]);
     }
 }
-async function updateData(Username, UserID, AuthKey, Shells, Win){
+async function updateData(Username, UserID, AuthKey, Shells, Win){ //updates user data in database
     try{
         var queryOne = `SELECT AuthKey FROM authentication WHERE Username = "${Username}";`;
         let selectResultOne = await query(queryOne);
@@ -279,7 +277,7 @@ async function updateData(Username, UserID, AuthKey, Shells, Win){
             if(authKeyResult == AuthKey){
                 var queryTwo = `SELECT * FROM player WHERE Username = "${Username}";`;
                 let selectResultTwo = await query(queryTwo);
-                if(Win == true){
+                if(Win == true){ //if user won the game
                     let wins = selectResultTwo[0].Wins;
                     let total = selectResultTwo[0].TotalScore;
                     wins++;
@@ -288,7 +286,7 @@ async function updateData(Username, UserID, AuthKey, Shells, Win){
                     await query(updateOne);
                     return[true];
                 }
-                else if(Win == false){
+                else if(Win == false){ //if user lost the game
                     let losses = selectResultTwo[0].Losses;
                     let total = selectResultTwo[0].TotalScore;
                     losses++;
@@ -309,33 +307,33 @@ async function updateData(Username, UserID, AuthKey, Shells, Win){
         return([false]);
     }
 }
-async function getLeaderboard(){
+async function getLeaderboard(){ //gets leaderboard data
     try{
         let result
-        var queryOne = `SELECT Username, TotalScore FROM player ORDER BY TotalScore DESC;`
+        var queryOne = `SELECT Username, TotalScore FROM player ORDER BY TotalScore DESC;` //get players in descending order of score
         let selectResultOne = await query(queryOne);
-        if(selectResultOne < 1){
+        if(selectResultOne < 1){ //if there are no users in database
             return["None", 0, "None", 0, "None", 0]
         }
-        else if(selectResultOne < 2){
+        else if(selectResultOne < 2){ //if only 1 person in database
             return[selectResultOne[0].Username, selectResultOne[0].TotalScore, "None", 0, "None", 0]
         }
-        else if(selectResultOne < 3){
+        else if(selectResultOne < 3){ //if only 2 people in database
             return[selectResultOne[0].Username, selectResultOne[0].TotalScore, 
             selectResultOne[1].Username, selectResultOne[1].TotalScore, "None", 0]
         }
-        else{
+        else{ //if 3 or more people in database
             return[selectResultOne[0].Username, selectResultOne[0].TotalScore, 
             selectResultOne[1].Username, selectResultOne[1].TotalScore,
              selectResultOne[2].Username, selectResultOne[2].TotalScore]
         }
     }
-    catch{
-
+    catch(err){
+        console.log(err)
     }
 }
 //PORT LISTEN START
-var httpsServer = https.createServer(credentials, app);
-httpsServer.listen(8888);
+var httpsServer = https.createServer(credentials, app); //https creation
+httpsServer.listen(8888); //run https server on port 8888
 console.log("Listening on port 8888");
 //API END
